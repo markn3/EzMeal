aba0a929b1544e95ccb66adea6af8ed07887a37e
Snps <Spagayon121@Gmail.com>
2021-03-28 14:19:05 -0500

    Create Menu.java

---
+++ EzMeal/Menu.java
@@ -0,0 +1,17 @@
+class Menu {
+
+    public void displayMainMenu() {
+        System.out.println("1. Log in");
+        System.out.println("2. Sign up");
+        System.out.println("3. Exit");
+        System.out.print("Please input your choice(1~3): ");
+    }
+
+    public void displayUserMenu() {
+        System.out.println("1. Search Recipes");
+        System.out.println("2. Try Something New");
+        System.out.println("3. Saved Recipes");
+        System.out.println("4. Log out");
+        System.out.print("Please input your choice(1~4): ");
+    }
+}
