36ac751e2acb81154493a21dc6d452d4518b20d8
Snps <Spagayon121@Gmail.com>
2021-03-28 14:19:08 -0500

    Create Recipe.java

---
+++ EzMeal/Recipe.java
@@ -0,0 +1,14 @@
+public class Recipe {
+    private String country;
+    private String mealTime;
+    
+    // list of ingredients needed so probably string array.
+    private String
+
+    private int time;
+    private int servings;
+
+    public Recipe(String c, String m, int t, int s) {
+    }
+
+}
